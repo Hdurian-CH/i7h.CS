08c43ac7375d05eb2b244606478b72a1bc547d6c
ZCH <chzhch2023@163.com>
2024-03-30 16:41:45 +0800

    First time

---
+++ i7h/i7h/Program.cs
@@ -0,0 +1,7 @@
+﻿using i7h;
+
+var t = "Alice was beginning to get very tired of sitting by her sister\non the bank, " +
+        "and of having nothing to do:  once or twice she had\npeeped into the book her sister was reading, " +
+        "but it had no\npictures or conversations in it, \\`and what is the use of a book,'\nthought Alice \\`without pictures or " +
+        "conversation?'";
+Console.WriteLine(t.i7h());
---
+++ i7h/i7h/i7hLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace i7h;
+
+public static class i7hLibrary
+{
+    public static string i7h(this string s)
+    {
+        return Regex.
+            Replace(s, 
+                @"\w+", 
+                r => r.Value.Length <= 2 ? r.Value : $"{r.Value[0]}{r.Value.Length - 2}{r.Value[^1]}");
+    }
+}
